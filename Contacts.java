10c51fd82b8ad15ce65a161d62319d4fd9f56c46
shivajighasti/04 <shivajighasti97@gmail.com>
2022-08-04 17:16:37 +0530

    Contact details of Person

---
+++ Contacts.java
@@ -0,0 +1,59 @@
+package com.bridgelabz.addressbook;
+
+/**
+ * contact details of person
+ * 
+ * @author Shivaji Ghasti
+ *
+ */
+public class Contacts {
+    private String firstName, lastName, email, mobileNumber;
+    Address address;
+
+    // Getter and Setter Methods
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getmobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setmobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact [First Name=" + firstName + ", Last Name=" + lastName + ", Email=" + email + ", Mobile Number="
+                + mobileNumber + " , " + address + "]";
+    }
+}
